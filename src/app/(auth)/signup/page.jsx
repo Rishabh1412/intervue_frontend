7494737e9abb735ebe086b5eff0f9bdfe0de7ac7
Rish <rishabhkartik2004@gmail.com>
2025-07-27 16:46:58 +0530

    SEO enhancements

--- src/app/(auth)/signup/page.jsx
+++ src/app/(auth)/signup/page.jsx
@@ -7,18 +7,6 @@ import { Bounce, ToastContainer, toast } from "react-toastify";
 
 // app/(auth)/signup/page.jsx
 
-export const metadata = {
-  title: "Sign Up",
-  description: "Create your free Intervue account to start conducting tech interviews in minutes.",
-  keywords: ["Sign Up", "Register", "Create Account", "Interview Platform"],
-  openGraph: {
-    title: "Sign Up | Intervue",
-    description: "Join Intervue and start hosting technical interviews with ease.",
-    url: "https://intervue-frontend-gamma.vercel.app/signup",
-    siteName: "Intervue",
-    type: "website",
-  },
-};
 
 const Page = () => {
   const router = useRouter();
